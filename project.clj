b81abefebcb6cb03c269333f09a56ddb14c36697
Ryan McGowan <ryan@ryanmcg.com>
2014-02-14 21:07:21 -0800

    Increment cegdown dep.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[incise-core "0.2.0-SNAPSHOT"]
-                 [me.raynes/cegdown "0.1.0"]]
+                 [me.raynes/cegdown "0.1.1"]]
   :repl-options {:init-ns incise.repl}
   :main incise.core)
