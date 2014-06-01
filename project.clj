b90b6aeecaf6f44528a5cf07fb1dd9ca9dff2e18
Ryan McGowan <ryan@ryanmcg.com>
2014-05-31 21:26:33 -0700

    Bump incise-core dep version and project version.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject incise-markdown-parser "0.1.0"
+(defproject incise-markdown-parser "0.2.0"
   :description "The default markdown parser for incise."
   :url "https://github.com/RyanMcG/incise-markdown-parser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[incise-core "0.2.0"]
+  :dependencies [[incise-core "0.3.2"]
                  [me.raynes/cegdown "0.1.1"]]
   :repl-options {:init-ns incise.repl}
   :main incise.core)
